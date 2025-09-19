6a2685acac98ae733e2d4adc3b5771747cc40737
sadumina <bagyasadumina.com>
2025-09-19 15:34:28 +0530

    refactor: Simplify API functions by removing async/await syntax

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,22 +1,12 @@
-// src/api.js
-
 import axios from "axios";
 
-const API_BASE = "https://haycarb-vehicle-detector.onrender.com/api";
+const API = "https://haycarb-vehicle-detector.onrender.com/api";
 
-export async function fetchVehicles() {
-  const res = await axios.get(`${API_BASE}/vehicles`);
-  return res.data;
-}
+// Get all vehicles
+export const fetchVehicles = () => axios.get(`${API}/vehicles`);
 
-export async function createVehicle(vehicleData) {
-  const res = await axios.post(`${API_BASE}/vehicles`, vehicleData, {
-    headers: { "Content-Type": "application/json" },
-  });
-  return res.data;
-}
+// Create vehicle
+export const createVehicle = (data) => axios.post(`${API}/vehicles`, data);
 
-export async function markVehicleExit(vehicleId) {
-  const res = await axios.put(`${API_BASE}/vehicles/${vehicleId}/exit`);
-  return res.data;
-}
+// Mark vehicle as exited
+export const markExit = (id) => axios.put(`${API}/vehicles/${id}/exit`);
